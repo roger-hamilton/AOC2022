bb24cf7ac6f83b0bbd78b7d4b6ae93fdf8d65b52
Roger Hamilton <roger.max.hamilton@gmail.com>
2022-12-21 08:56:19 -0600

    formatting

--- Day21/solve.py
+++ Day21/solve.py
@@ -4,6 +4,7 @@ import cvxpy as cp
 
 Input = dict[str, str]
 
+
 def read_input(fname: str) -> Input:
     fname = os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)
     with open(fname, encoding='UTF8') as f:
@@ -17,6 +18,7 @@ ops: dict[str, Callable] = {
     '/': lambda x, y: x / y,
     '==': lambda x, y: x == y,
 }
+
 
 def resolve(val: str, data: Input, calc_terminal: Callable[[str], any]):
     term = calc_terminal(val)
@@ -32,13 +34,16 @@ def resolve(val: str, data: Input, calc_terminal: Callable[[str], any]):
 
     return ops[op](left, right)
 
+
 def part1(data: Input):
     return int(resolve(data['root'], inp, lambda val: int(val) if val.isdecimal() else None))
+
 
 def part2(data: Input):
     humn = cp.Variable(name='humn', integer=True)
     root_left, _, root_right = data['root'].split(' ')
-    constraint = resolve(f'{root_left} == {root_right}', data, lambda val: humn if val == 'humn' else cp.Constant(int(val)) if val.isdecimal() else None)
+    constraint = resolve(f'{root_left} == {root_right}', data, lambda val: humn if val ==
+                         'humn' else cp.Constant(int(val)) if val.isdecimal() else None)
     return int(cp.Problem(cp.Maximize(humn), [constraint]).solve())
 
 
